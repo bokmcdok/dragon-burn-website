52861c7ed6f3231c885414517e5a99c3fc56db3a
Mark McDonagh <theoddman@theoddman.com>
2016-12-20 18:31:39 +0800

    Added image of the day placeholder.

--- sidebar-home.php
+++ sidebar-home.php
@@ -1,2 +1,17 @@
 <?php
+/**
+ * The template for the sidebar containing the main widget area for the home page
+ */
 ?>
+
+<?php if ( is_active_sidebar( 'sidebar-1' )  ) : ?>
+	<aside id="secondary" class="sidebar widget-area" role="complementary">
+		<div class="home-sidebar-top">
+			<h1>From the Gallery</h1>
+			<?php if ( function_exists( 'soliloquy' ) ) { soliloquy( '679' ); } ?>
+			<div class="home-sidebar-top-link">
+				<a href="http://www.dragonburn.bokmcdok.com/en/gallery/image-gallery/">Visit the Image Gallery</a>
+			</div>
+		</div>
+	</aside><!-- .sidebar .widget-area -->
+<?php endif; ?>
