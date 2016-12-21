699d526b7632fc71f137d4fde2cc657308b82cb8
Mark McDonagh <theoddman@theoddman.com>
2016-12-21 16:31:10 +0800

    Added Guiding Principles

--- sidebar-home.php
+++ sidebar-home.php
@@ -13,5 +13,15 @@
 				<a href="http://www.dragonburn.bokmcdok.com/en/gallery/image-gallery/">Visit the Image Gallery</a>
 			</div>
 		</div>
+		
+		<div class="home-sidebar-bottom">
+			<h1>Guiding Principles</h1>
+			<p>The Dragon Burn community's Guiding Principles are based on the
+			Ten Principles of Burning Man. They include  <b>Gifting</b>,
+			<b>Radical Free Expression</b> and <b>Leaving No Trace</b>.</p>
+			<div class="home-sidebar-bottom-link">
+				<a href="http://www.dragonburn.bokmcdok.com/en/about/dragon-burn-works/guiding-principles/">See Our Guiding Principles</a>
+			</div>
+		</div>
 	</aside><!-- .sidebar .widget-area -->
 <?php endif; ?>
