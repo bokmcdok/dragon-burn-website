89090d1535082a89d636fe1553b5603be51e18b3
Mark McDonagh <theoddman@theoddman.com>
2017-01-11 15:08:41 +0800

    excerpts now work for Chinese articles

--- functions.php
+++ functions.php
@@ -40,45 +40,48 @@ add_action( 'wp_enqueue_scripts', 'dragon_dequeue_google_fonts', 20 );
 function improved_trim_excerpt($text) {
         global $post;
         if ( '' == $text ) {
-				//  Get default content.
-                $text = get_the_content('');
-                $text = apply_filters('the_content', $text);
-                $text = str_replace('\]\]\>', ']]&gt;', $text);
-
-				//  Filter out any scripts.
-                $text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
-
-				//  Strip out HTML but keep subheadings and paragraphs.
-                $text = strip_tags($text, '<p><h2>');
-
-				//  Increase the length to 15 words.
-                $excerpt_length = 25;
-                $words = explode(' ', $text, $excerpt_length + 1);
-                if (count($words)> $excerpt_length) {
-                        array_pop($words);
-                        $text = implode(' ', $words);
-                }
-
-		//  Cut off after the last complete sentence.
-		$sentences = explode('.', $text);		
-		if (sizeof($sentences) > 1)
-		{
-			array_pop($sentences);
-			$text = implode('.', $sentences);
-		}
-		else
-		{
-			$text .= "...";
-		}
-
-		//  Add a 'Read More...' link.
-		$text = $text.'.</p><p><a href="'.get_permalink().'">Read More...</a></p>';
-
-		//  Include the thumbnail if there is one.
-		if ( has_post_thumbnail() )
-		{
-			$text = the_post_thumbnail().$text;
-		}
+			//  Get default content.
+	        $text = get_the_content('');
+	        $text = apply_filters('the_content', $text);
+	        $text = str_replace('\]\]\>', ']]&gt;', $text);
+	
+			//  Filter out any scripts.
+	        $text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
+			
+			//  Strip out HTML but keep subheadings and paragraphs.
+	        $text = strip_tags($text, '<p><h2>');
+	
+			//  Increase the length to 25 words.
+            $excerpt_length = 25;
+                        
+            if (function_exists("pll_current_language") &&
+            	pll_current_language() == "zh")
+            {
+            	$text = mb_substr($text, 0, $excerpt_length);
+            }
+            else
+            {
+	            $words = explode(' ', $text, $excerpt_length + 1);
+	            if (count($words)> $excerpt_length) {
+	                    array_pop($words);
+	                    $text = implode(' ', $words);
+	            }
+			}
+			
+			//  Cut off after the last complete sentence.
+			$sentences = explode('.', $text);		
+			if (sizeof($sentences) > 1)
+			{
+				array_pop($sentences);
+				$text = implode('.', $sentences);
+			}
+			else
+			{
+				$text .= "...";
+			}
+	
+			//  Add a 'Read More...' link.
+			$text = $text.'.</p><p><a href="'.get_permalink().'">Read More...</a></p>';
         }
 
         return $text;
@@ -93,7 +96,14 @@ add_filter('get_the_excerpt', 'improved_trim_excerpt');
  */
 
 function db_get_base_url() {
-	return esc_url( site_url( '/' ) . pll_current_language() );
+	if (function_exists("pll_current_language"))
+	{
+		return esc_url( site_url( '/' ) . pll_current_language() );
+	}
+	else
+	{
+		return esc_url( site_url( '/'));
+	}
 }
 
 ?>
